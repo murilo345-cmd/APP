27814c47f35a8f25ea27d39ec4f3e21826d6005b
ALUNO - SL103 <sl103@escolasebraesp.local>
2026-04-10 12:29:37 -0300

    V1 APP mobile

---
+++ app/src/main/java/com/example/applogin/service/ApiService.kt
@@ -0,0 +1,15 @@
+package com.example.applogin.service
+
+import com.example.applogin.model.Usuario
+import retrofit2.Call
+import retrofit2.http.Body
+import retrofit2.http.POST
+
+interface ApiService {
+
+    @POST( "/cadastro")
+    fun cadastrarUsuario(
+       @Body usuario: Usuario
+    ): Call<Usuario>
+
+}
